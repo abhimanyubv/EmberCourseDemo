364eb011b33db5aff8da89108a406cc953b43bba
Abhimanyu Bondada <vmabhi453@gmail.com>
2014-09-23 15:19:19 +0530

    assets changes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -16,7 +16,7 @@ set :stage, :production
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server '192.168.10.12', user: 'deploy', roles: %w{web app}
+server '192.168.10.12', user: 'deploy', roles: %w{web app db}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
