832288938c46aa32b0924cdc93a15493d05c4c00
Abhimanyu Bondada <vmabhi453@gmail.com>
2014-09-23 16:54:00 +0530

    deploy user configured for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,9 @@ set :deploy_to, '/home/deploy/ember_demo'
 
 # Default value for :scm is :git
 set :scm, :git
+set :deploy_via, :remote_cache
+
+set :user, "deploy"
 
 # Default value for :format is :pretty
 # set :format, :pretty
