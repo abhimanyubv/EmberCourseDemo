fec94ab09ea99946dcbe36e581b94987e9e6c265
Abhimanyu Bondada <vmabhi453@gmail.com>
2014-09-25 15:02:34 +0530

    ssh configuration updated

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -16,7 +16,16 @@ set :stage, :production
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server '192.168.10.12', user: 'deploy', roles: %w{web app db}
+# server '192.168.10.12', user: 'deploy', roles: %w{web app db}
+
+server '192.168.10.12', roles: [:web, :app, :db], primary: true, ssh_options: {
+    user: "deploy",
+    port: 5498,
+    forward_agent: true,
+    password: 'abhimanyu',
+    auth_methods: %w(publickey)
+}
+
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
